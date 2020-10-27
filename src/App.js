c430347e15459d45d0ba8d886e6962a6d956e5f5
Ahmed Nawaz Khan <ahmed@techimplement.com>
2020-10-27 14:01:17 +0500

    fetching content

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      monsters: []
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.monsters.map(monster => <h1 key={monster.id}>{monster.name}</h1>)}
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters: users}));
+  }
 }
 
 export default App;
